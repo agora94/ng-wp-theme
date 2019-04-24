ae3f2095fe5a0930c9bf1b9daff9494714e60350
Andrew Gora <gora.andrew@gmail.com>
2019-04-24 11:49:31 -0700

    updated to angular 7; compiles and serves but still needs more testing

--- src/app/posts/post-single/post-single.component.ts
+++ src/app/posts/post-single/post-single.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
+import { switchMap } from 'rxjs/operators';
 import { Post } from '../post';
-import 'rxjs/add/operator/switchMap';
+
 
 @Component({
   selector: 'app-post-single',
@@ -20,9 +21,10 @@ export class PostSingleComponent implements OnInit {
   ngOnInit() {
 
     this.route.paramMap
-    .switchMap((params: ParamMap) =>
+    .pipe(
+      switchMap((params: ParamMap) =>
       this.postsService.getPost(params.get('slug')))
-    .subscribe(
+    ).subscribe(
       (post: Post[]) => this.post = post[0],
       (err: HttpErrorResponse) => err.error instanceof Error ? console.log('An error occurred:', err.error.message) : console.log(`Backend returned code ${err.status}, body was: ${err.error}`)
     );
